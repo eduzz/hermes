b387a0eb552141c2c5b325d5a4799f2653b1236e
Carlos André Ferrari <caferrari@gmail.com>
2019-05-31 11:22:55 -0300

    fixed keyParser to allow hifens and underscores

--- src/RoutingKeyParser.php
+++ src/RoutingKeyParser.php
@@ -6,6 +6,6 @@ class RoutingKeyParser
 {
     public static function isValid($routingKey)
     {
-        return preg_match('@^[a-z]+\.[a-z]+\.[a-z]+$@', $routingKey);
+        return preg_match('@^[a-z_\-]+\.[a-z_\-]+\.[a-z_\-]+$@', $routingKey);
     }
 }
